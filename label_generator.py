7a7228b9bd7a5aace93b4aab296f32f9598d3dc6
colbyendres <colby.endres@tamu.edu>
2025-04-27 19:21:56 -0500

    Removed unnecessary files

--- label_generator.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import os
-from sentence_transformers import SentenceTransformer
-
-class LabelGenerator:
-    def __init__(paper_labels, label_path):
-        model = SentenceTransformer('all-MiniLM-L6-v2')
-        embeddings = model.encode(paper_labels)
-        np.save(os.path.join(label_path, 'paper_embeddings.npy'), embeddings)
